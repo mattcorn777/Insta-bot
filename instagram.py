be2a9ec8aa22785d22f2d373d88060c1ef53d214
mattcorn777 <mrc5@utas.edu.au>
2017-09-05 22:21:50 +1000

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,6 +1,9 @@
 #Created by Matt Cornwell
 
 from login import *
+from likes import *
 
+driver = webdriver.Chrome(executable_path = '/usr/local/bin/chromedriver')
 
-login()
+login(driver)
+likePhoto(driver)
