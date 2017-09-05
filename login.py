5bd4941e4a71d16cc4c01be596afc83e423a30f9
mattcorn777 <mrc5@utas.edu.au>
2017-09-05 22:20:57 +1000

    Update login.py

--- login.py
+++ login.py
@@ -4,12 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-def login():
+def login(driver):
 
     user = '' #Enter your username here
     pwd = '' #Enter your password here
     
-    driver = webdriver.Chrome(executable_path = '/usr/local/bin/chromedriver')
     driver.get('https://www.instagram.com/accounts/login/');
 
     username = driver.find_element_by_name('username')
@@ -20,4 +19,4 @@ def login():
 
     password.send_keys(Keys.RETURN)
 
-    return;
+    return driver;
